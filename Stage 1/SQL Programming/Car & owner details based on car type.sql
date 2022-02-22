5de7468486d4a756cb5798ceaac35a389f395105
Sujan-DVSS <63912098+Sujan-DVSS@users.noreply.github.com>
2022-02-22 17:23:53 +0530

    Update Car & owner details based on car type.sql

--- Stage 1/SQL Programming/Car & owner details based on car type.sql
+++ Stage 1/SQL Programming/Car & owner details based on car type.sql
@@ -1,1 +1,3 @@
-select car_id,car_name,owner_id from cars where car_type="Hatchback" or car_type="SUV" order by car_id;
+--select car_id,car_name,owner_id from cars where car_type="Hatchback" or car_type="SUV" order by car_id;
+--or
+select car_id, car_name, owner_id from cars where car_type IN ("Hatchback","SUV");
